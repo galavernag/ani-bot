22561c7ffdfc021b73166f52b67a766000431dd0
Guilherme Galaverna <guigalaverna@gmail.com>
2024-06-06 22:35:09 -0300

    feat: create ping command

---
+++ src/commands/utility/ping.ts
@@ -0,0 +1,9 @@
+import { CommandInteraction, SlashCommandBuilder } from "discord.js";
+
+const data = new SlashCommandBuilder()
+  .setName("ping")
+  .setDescription("Replies with pong!");
+
+async function execute(interaction: CommandInteraction) {
+  await interaction.reply("pong!");
+}

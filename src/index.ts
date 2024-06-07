af5829827c6c9415945a9702210aff2e9ec5ff08
Guilherme Galaverna <guigalaverna@gmail.com>
2024-06-06 23:36:15 -0300

    feat: handling interactions

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,33 @@
-import { Client, Events, GatewayIntentBits } from "discord.js";
+import {
+  Client,
+  Collection,
+  CommandInteraction,
+  Events,
+  GatewayIntentBits,
+} from "discord.js";
+import { deploy_commands } from "./deploy";
+import type { Command } from "./types/command";
 
 const token = process.env.TOKEN;
 
-function main() {
+async function main() {
   const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+  client.commands = new Collection();
+  await deploy_commands(client);
 
   client.once(Events.ClientReady, ready_client => {
     console.log(`Bot is running as ${ready_client.user.tag}`);
+  });
+
+  client.on(Events.InteractionCreate, async interaction => {
+    if (interaction.isCommand()) {
+      const execute = client.commands.get(interaction.commandName) as (
+        interaction: CommandInteraction
+      ) => Promise<void>;
+
+      await execute(interaction);
+    }
   });
 
   client.login(token);
